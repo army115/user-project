0d3af6def95cb09f42002a158b62607a73a2ef0e
army115 <89437364+army115@users.noreply.github.com>
2021-10-29 14:36:04 +0700

    hot fix

--- lib/model/Connectapi.dart
+++ lib/model/Connectapi.dart
@@ -1,6 +1,6 @@
 class Connectapi {
   
-  String domain = 'http://192.168.43.65:8888';
+  String domain = 'http://192.168.1.15:8888';
   String orImageDomain = 'http://192.168.43.65:8888/uploads/images/';
   String checkImagesDomain = 'http://192.168.43.65:8888/uploads/imagescheck/';
   String payImagesDomain = 'http://192.168.43.65:8888/uploads/imagespayment/';
